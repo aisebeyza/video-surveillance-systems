3147974271a9d513683a5ad17d507801065674ea
aisebeyza <67642654+aisebeyza@users.noreply.github.com>
2021-01-07 16:32:32 +0300

    edit signout

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,34 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:video_surveillance_systems/components/bottom_navigation_bar.dart';
 import 'package:video_surveillance_systems/screens/camera/camera_screen.dart';
+import 'package:video_surveillance_systems/screens/sign_in/sign_in_screen.dart';
 import '../../constants.dart';
 import '../../enums.dart';
 import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   static String routeName = "/home";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: kPrimaryColor,
-          centerTitle: true,
-          title: Text(
-            "Ana Sayfa",
-            style: TextStyle(color: Colors.black),
+        automaticallyImplyLeading: false,
+        backgroundColor: kPrimaryColor,
+        centerTitle: true,
+        title: Text(
+          "Ana Sayfa",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              icon: Icon(Icons.login),
+              onPressed: () async {
+                await _auth.signOut();
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text("Başarıyla çıkış yapıldı"),
+                ));
+                Navigator.pushNamed(context, SignInScreen.routeName);
+              },
+            ),
           ),
-          actions: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(right: 20.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Icon(
-                    Icons.exit_to_app,
-                    size: 26.0,
-                  ),
-                )),
-          ]),
+        ],
+      ),
       body: Body(),
       bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
